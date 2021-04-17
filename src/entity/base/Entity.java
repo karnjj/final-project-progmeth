73aaca7c9b501c04f968cad14c0de4f4585e2366
Karnjj <karnjj@hotmail.com>
2021-04-17 20:37:38 +0700

    ✨create entity class

---
+++ src/entity/base/Entity.java
@@ -0,0 +1,35 @@
+package entity.base;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public abstract class Entity {
+    private double x;
+    private double y;
+
+    public Entity(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillOval(x,y,25,25);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
