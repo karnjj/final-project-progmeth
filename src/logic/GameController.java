509a764055dbec3b3b5de0852c6e9a1e0d7abd81
Karnjj <karnjj@hotmail.com>
2021-05-22 14:26:47 +0700

    ♻clean up code

--- src/logic/GameController.java
+++ src/logic/GameController.java
@@ -18,9 +18,9 @@ public class GameController {
     private static GameState gameState;
     private static EntityManager entityManager;
     private static boolean isWin;
-   
-    private static Turret heroTurret = new HeroTurret(150,155);
-    private static Turret enemyTurret = new EnemyTurret(2850,155);
+
+    private static Turret heroTurret = new HeroTurret(150, 155);
+    private static Turret enemyTurret = new EnemyTurret(2850, 155);
 
     public static void InitGame() {
         bot = new Bot();
@@ -28,13 +28,13 @@ public class GameController {
         entityManager = new EntityManager();
         gameState = GameState.Home;
         isWin = false;
-        entityManager.addEntities(heroTurret,enemyTurret);
+        entityManager.addEntities(heroTurret, enemyTurret);
     }
 
     public static void clear() {
         entityManager.clear();
-        heroTurret = new HeroTurret(150,155);
-        enemyTurret = new EnemyTurret(2850,155);
+        heroTurret = new HeroTurret(150, 155);
+        enemyTurret = new EnemyTurret(2850, 155);
     }
 
     public static int getCurrentEnergy() {
@@ -44,7 +44,7 @@ public class GameController {
     public static int getMxEnergy() {
         return energy.getMxEnergy();
     }
-    
+
     public static void useEnergy(int energy) {
         if (!GameController.energy.Use(energy)) {
             System.out.println("error not enought");
@@ -67,33 +67,33 @@ public class GameController {
         entityManager.cleanupEntities();
         for (Entity e : entityManager.getAllEntity()) {
             e.update(dt);
-            if(e.getState() == logic.State.DEAD) entityManager.addEntitiesToBeRemoved(e);
+            if (e.getState() == logic.State.DEAD) entityManager.addEntitiesToBeRemoved(e);
             if (e instanceof Movable) {
-                if(e.getState() == logic.State.WALK) ((Movable) e).move(dt);
+                if (e.getState() == logic.State.WALK) ((Movable) e).move(dt);
             }
             if (e instanceof Attackable) {
-                if(e.getState() == logic.State.ATTACK && ((Attackable) e).canAttack()) {
-                Entity target = getFrontmost(e.getSide().getOpposite());
-                if (target != null) ((Attackable) e).attack((Damageable) target);
-            }
+                if (e.getState() == logic.State.ATTACK && ((Attackable) e).canAttack()) {
+                    Entity target = getFrontmost(e.getSide().getOpposite());
+                    if (target != null) ((Attackable) e).attack((Damageable) target);
+                }
             }
         }
     }
-    
-    public static void createRanger(String name,Side side) {
-    	Ranger ranger;
-    	SoundUtils.createdRanger();
-	    double x = side == Side.HERO ? 200 : Drawing.getGameWidth() - 200;
-	    double y = getRandomNumber(
-	            Drawing.getWindowHeight() - 70,
+
+    public static void createRanger(String name, Side side) {
+        Ranger ranger;
+        SoundUtils.createdRanger();
+        double x = side == Side.HERO ? 200 : Drawing.getGameWidth() - 200;
+        double y = getRandomNumber(
+                Drawing.getWindowHeight() - 70,
                 Drawing.getWindowHeight()
-                );
-    	switch (name) {
-    	    case "Inkblue" -> ranger = new Inkblue(x,y,side);
-    	    case "Slime" -> ranger = new Slime(x,y,side);
-    	    case "Minotaur" -> ranger = new Minotaur(x,y,side);
-    	    case "Alien" -> ranger = new Alien(x,y,side);
-    	    case "Inkred" -> ranger = new Inkred(x,y,side);
+        );
+        switch (name) {
+            case "Inkblue" -> ranger = new Inkblue(x, y, side);
+            case "Slime" -> ranger = new Slime(x, y, side);
+            case "Minotaur" -> ranger = new Minotaur(x, y, side);
+            case "Alien" -> ranger = new Alien(x, y, side);
+            case "Inkred" -> ranger = new Inkred(x, y, side);
             default -> throw new IllegalStateException("Unexpected value: " + name);
         }
         entityManager.addEntities(ranger);
@@ -106,13 +106,13 @@ public class GameController {
         double minX = Double.MAX_VALUE;
         for (Entity e : entityManager.getAllEntity()) {
             if (e instanceof Damageable) {
-                if ( e.getSide() == Side.HERO) {
+                if (e.getSide() == Side.HERO) {
                     if (maxX < e.getX()) {
                         maxX = e.getX();
                         frontHero = e;
                     }
                 }
-                if ( e.getSide() == Side.ENEMY) {
+                if (e.getSide() == Side.ENEMY) {
                     if (minX > e.getX()) {
                         minX = e.getX();
                         frontEnemy = e;
@@ -124,13 +124,13 @@ public class GameController {
     }
 
 
-	public static GameState getGameState() {
-		return gameState;
-	}
+    public static GameState getGameState() {
+        return gameState;
+    }
 
-	public static void setGameState(GameState gameState) {
-		GameController.gameState = gameState;
-	}
+    public static void setGameState(GameState gameState) {
+        GameController.gameState = gameState;
+    }
 
     public static int getRandomNumber(int min, int max) {
         Random random = new Random();
@@ -149,33 +149,27 @@ public class GameController {
         return GameController.enemyTurret;
     }
 
-	public static boolean isWin() {
-		return isWin;
-	}
-	
-	public static boolean isGameOver() {
-		if(getHeroTurret().getCurrentHP()<=0) {
-			setWin(false);
-			Drawing.updatePanel(GameState.End);
-			return true;
-		}
-		if(getEnemyTurret().getCurrentHP()<=0) {
-			setWin(true);
-			Drawing.updatePanel(GameState.End);
-			return true;
-		}
-		return false;
-	}
+    public static boolean isWin() {
+        return isWin;
+    }
 
-	public static void setWin(boolean isWin) {
-		GameController.isWin = isWin;
-	}
-    
-    
+    public static boolean isGameOver() {
+        if (getHeroTurret().getCurrentHP() <= 0) {
+            setWin(false);
+            Drawing.updatePanel(GameState.End);
+            return true;
+        }
+        if (getEnemyTurret().getCurrentHP() <= 0) {
+            setWin(true);
+            Drawing.updatePanel(GameState.End);
+            return true;
+        }
+        return false;
+    }
 
-	
-	
-	
-    
-    
+    public static void setWin(boolean isWin) {
+        GameController.isWin = isWin;
+    }
+
+
 }
