4e36147506f71acf2ebd8d8333e20a14155b52f0
Karnjj <karnjj@hotmail.com>
2021-05-23 16:20:49 +0700

    ✨update interface

--- src/entity/base/Attackable.java
+++ src/entity/base/Attackable.java
@@ -3,5 +3,4 @@ package entity.base;
 public interface Attackable {
     public abstract void attack(Damageable e);
     public abstract boolean canAttack();
-    public abstract int getAttackRange();
 }
--- src/entity/base/Movable.java
+++ src/entity/base/Movable.java
@@ -1,6 +1,5 @@
 package entity.base;
 
 public interface Movable {
-    public abstract int getSpeed();
     public abstract void move(double dt);
 }
